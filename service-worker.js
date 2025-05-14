243c75058c64800e01ce70a7a8cf2e33b109fa3d
Aditya Tiwari <atiwar0414@gmail.com>
2025-05-13 22:55:19 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,40 @@
-// Basic service worker with minimal caching
 self.addEventListener('install', (event) => {
-  console.log('Service worker installing...');
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open('mindmatters-cache-v1').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activating...');
-  event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        caches.open('mindmatters-cache-v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/offline.html',
+                '/styles/main.css',
+                '/favicon.ico',
+                '/manifest.json',
+                // Add other assets to cache as needed
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => {
+                return caches.match('/offline.html');
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['mindmatters-cache-v1'];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
